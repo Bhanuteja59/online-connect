107a474a848d4818dcbb4c0ddad9045e1339d50d
Bunny <22am1a3118@svrec.ac.in>
2025-05-31 11:28:22 +0530

    Add STATIC_ROOT setting for collectstatic

--- onlineWedding/settings.py
+++ onlineWedding/settings.py
@@ -109,11 +109,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
 import os
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
